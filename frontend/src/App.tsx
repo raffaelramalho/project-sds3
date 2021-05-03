2d88974cd81cc6eecf297f9239de9ac511793889
OkitaHokusai <21800014@aluno.cotemig.com.br>
2021-05-03 15:07:57 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,30 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-      <h1 className="text-primary">Olá Mundo</h1>
-      <DataTable/>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+        </div>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
