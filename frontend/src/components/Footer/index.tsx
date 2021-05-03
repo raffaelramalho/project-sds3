742067f4104be282f8f7871a9819f38febf6ee01
OkitaHokusai <21800014@aluno.cotemig.com.br>
2021-05-03 14:38:30 -0300

    Basic Static

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/okitahokusai" target="_blank" rel="noreferrer">Rafael Ramalho</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br /></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
